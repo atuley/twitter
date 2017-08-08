82374aa0dba9256b60fd9f5e8ea8370715a84d92
Alex Tuley <alex.intern@gaslight.co>
2017-08-08 10:14:35 -0400

    Add controller and more feature tests

--- spec/controllers/tweet_controller_spec.rb
+++ spec/controllers/tweet_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TweetController, type: :controller do
+  let!(:valid_tweet) { build(:tweet) }
 
   describe "GET #new" do
     it "returns http success" do
@@ -9,4 +10,13 @@ RSpec.describe TweetController, type: :controller do
     end
   end
 
+  # How to test my controller properly
+  # describe "POST #create" do
+  #   it "with valid tweet info" do
+  #     post :create, { tweet: { content: valid_tweet.content } }
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # test index
 end
--- spec/features/tweet_spec.rb
+++ spec/features/tweet_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 feature 'Create tweet' do
   let!(:valid_tweet) { build(:tweet) }
+  let!(:invalid_tweet) { build(:tweet, content: '') }
 
   scenario 'with valid information' do
     tweet_page.visit_create_tweet
     tweet_page.fill_in_tweet(valid_tweet)
     tweet_page.submit
 
-    # assert curent path is home
-    # assert tweet was added to show liste
-    # controller tests!
+    expect(tweet_page).to have_text(valid_tweet.content)
+    expect(current_path).to eq("/")
+  end
+
+  scenario 'with invalid information' do
+    tweet_page.visit_create_tweet
+    tweet_page.fill_in_tweet(invalid_tweet)
+    tweet_page.submit
+
+    expect(current_path).to eq("/tweet/new")
   end
 end
