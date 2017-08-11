ad1145019986800f940408539b37460519c64bc0
Alex Tuley <alex.intern@gaslight.co>
2017-08-11 11:24:27 -0400

    Not let user create tweet if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
   end
 
   helper_method :current_user
+
+  def authorize
+    redirect_to login_path, alert: "Not authorized" if current_user.nil?
+  end
 end
