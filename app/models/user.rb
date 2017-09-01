de070ea571ef94d0f91af96ed425af306cfb5ddb
Alex Tuley <alex.intern@gaslight.co>
2017-08-31 21:16:54 -0400

    Add ability to follow and unfollow a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,27 @@
 class User < ApplicationRecord
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
+
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   has_secure_password
 
   validates :email, :password, :password_confirmation, presence: true
   validates :email, format: { with: %r{.+@.+\..+} }
   validates :password, length: { within: 6..40 }
+
+  def follow(other)
+    active_relationships.create(followed_id: other.id)
+  end
+
+  def unfollow(other)
+    active_relationships.find_by(followed_id: other.id).destroy
+  end
+
+  def following?(other)
+    following.include?(other)
+  end
 end
