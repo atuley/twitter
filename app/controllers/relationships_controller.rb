b88538956cda2750f4e8a70bd25bca2f80e0a6f5
Alex Tuley <alex.intern@gaslight.co>
2017-08-31 23:47:32 -0400

    Spike on follower show page

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,17 @@
 class RelationshipsController < ApplicationController
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
     redirect_to(:back)
   end
 
   def destroy
-    user = User.find(params[:followed_id])
-    current_user.unfollow(user)
+    @user = User.find(params[:followed_id])
+    current_user.unfollow(@user)
     redirect_to(:back)
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
