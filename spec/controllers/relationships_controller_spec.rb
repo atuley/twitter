39fd6f3e56d75a59cfa2721eafc5fb6607fa995f
Alex Tuley <alex.intern@gaslight.co>
2017-10-04 16:28:27 -0400

    WIP

---
+++ spec/controllers/relationships_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe RelationshipsController, type: :controller do
+  let!(:user) { create(:user) }
+  let!(:followed_user) {create(:user, {email: "foo2@bar.com"})}
+
+  describe "POST #create" do
+    it "redirects_back" do
+      allow(controller).to receive(:current_user) { user }
+      post :create, params: {followed_id: followed_user.id}
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  # TODO: left off here
+  describe "DELETE #destroy" do
+    xit "deletes session refreshes page" do
+      allow(controller).to receive(:current_user) { user }
+      delete :destroy, params: {followed_id: followed_user.id, id: user.id}
+      expect(response).to have_http_status(302)
+    end
+  end
+end
