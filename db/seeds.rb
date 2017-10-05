360be3131d8363a1f05a3c401cdfd6d06ae89c69
Alex Tuley <alex.intern@gaslight.co>
2017-10-05 16:29:21 -0400

    Add basic seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!("email"=>"foo@bar.com", "password" => "gaslight123", "password_confirmation" => "gaslight123")
+User.create!("email"=>"foo2@bar.com", "password" => "gaslight123", "password_confirmation" => "gaslight123")
+
+Tweet.create!("content"=>"This is my tweet", "user_id"=>1)
+Tweet.create!("content"=>"hello\r\n", "user_id"=>2)
+Tweet.create!("content"=>"\" \"", "user_id"=>1)
+
+Relationship.create!("follower_id"=>2, "followed_id"=>1)
+Relationship.create!("follower_id"=>1, "followed_id"=>2)
