491f444f80fc13ab624240e3b3cf12cde4421ae2
Alex Tuley <alex.intern@gaslight.co>
2017-10-08 15:12:23 -0500

    Add delete option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def get_favorite(tweet_id)
+    current_user.favorites.find_by(tweet_id: tweet_id)
+  end
+
+  def tweet_favored?(tweet_id)
+	  !get_favorite(tweet_id).nil?
+  end
+
+  helper_method :tweet_favored?
+  helper_method :get_favorite
+
+
   private
 
   def current_user
