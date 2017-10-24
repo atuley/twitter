c284deb3e2ceb8a6aee50cdcffc3696379a76716
Alex Tuley <alex.intern@gaslight.co>
2017-10-24 15:19:10 -0400

    Move authorization function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def authorize
+    redirect_to login_path, alert: "Not authorized" if current_user.nil?
+  end
+
   def get_favorite(tweet_id)
     current_user.favorites.find_by(tweet_id: tweet_id)
   end
@@ -19,8 +23,4 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :tweet_favored?
   helper_method :get_favorite
-
-  def authorize
-    redirect_to login_path, alert: "Not authorized" if current_user.nil?
-  end
 end
